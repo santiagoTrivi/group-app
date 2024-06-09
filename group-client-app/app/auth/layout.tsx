6c849367006333fb362abc72598f0b8a404322f3
santiagoTrivi <santiagocarvajal103@gmail.com>
2024-06-09 19:17:05 -0400

    feat(homepage): created

---
+++ group-client-app/app/auth/layout.tsx
@@ -0,0 +1,19 @@
+import { FC, ReactNode } from "react";
+
+interface AuthLayoutProps {
+    children: ReactNode
+}
+
+const AuthLayout: FC<AuthLayoutProps> = ({children}) => {
+
+    return (
+       <main className="grid grid-cols-1 lg:grid-cols-2">
+        <div >
+            layout
+        </div>
+        <div>{children}</div>
+       </main>
+    );
+}
+
+export default AuthLayout;
