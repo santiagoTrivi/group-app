6ac227f42ed3bae448076a2e713126bd98bd6ab4
santiagoTrivi <santiagocarvajal103@gmail.com>
2024-07-13 15:31:34 -0400

    update(backend): cors

--- group-backend/src/config/cors.config.ts
+++ group-backend/src/config/cors.config.ts
@@ -1,7 +1,11 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 export const corsConfig: CorsOptions = {
-  origin: ['http://localhost:3000', 'http://localhost:3001'],
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'https://group-dmipqb41e-santiagotrivis-projects.vercel.app',
+  ],
   preflightContinue: false,
   methods: ['*', 'PATCH'],
   optionsSuccessStatus: 204,
